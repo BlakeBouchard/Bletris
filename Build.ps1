0411e59ebcd37e44564054ecba383ce5d644dee2
Blake Bouchard <1417990+BlakeBouchard@users.noreply.github.com>
2021-10-06 12:10:57 -0600

    Preliminary MacOS build settings
    * Note that it still doesn't build on MacOS because GameWindow currently relies heavily on Windows APIs
    * May need to fix some stuff on Windows after pushing this

--- Build.ps1
+++ Build.ps1
@@ -1,40 +1,62 @@
 param ([switch] $Clean = $false, [switch] $Rebuild = $false, [switch] $Release = $false)
 
-if ($null -eq (Get-Command "nmake.exe" -ErrorAction SilentlyContinue))
+if ($IsWindows)
 {
-	# This script will fail if it's not run from a Visual Studio Developer command line
-	# If nmake is not on the path, we can assume that we need to enter the VS Command line
-	$vsPath = &(Join-Path ${env:ProgramFiles(x86)} "\Microsoft Visual Studio\Installer\vswhere.exe") -property installationpath
-	Write-Output "Using Visual Studio path: $vsPath"
-	Import-Module (Get-ChildItem $vsPath -Recurse -File -Filter Microsoft.VisualStudio.DevShell.dll).FullName
-	Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation
-}
-
-# We just want to run nmake here, and use the makefile for our complicated stuff
-
-if ($Clean)
-{
-	Invoke-Expression "nmake clean"
-}
-elseif ($Release)
-{
-	if ($Rebuild)
+	if ($null -eq (Get-Command "nmake.exe" -ErrorAction SilentlyContinue))
 	{
-		Invoke-Expression "nmake rebuild-release"
+		# This script will fail if it's not run from a Visual Studio Developer command line
+		# If nmake is not on the path, we can assume that we need to enter the VS Command line
+		$vsPath = &(Join-Path ${env:ProgramFiles(x86)} "\Microsoft Visual Studio\Installer\vswhere.exe") -property installationpath
+		Write-Output "Using Visual Studio path: $vsPath"
+		Import-Module (Get-ChildItem $vsPath -Recurse -File -Filter Microsoft.VisualStudio.DevShell.dll).FullName
+		Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation
+	}
+
+	# We just want to run nmake here, and use the makefile for our complicated stuff
+	if ($Clean)
+	{
+		Invoke-Expression "nmake clean"
+	}
+	elseif ($Release)
+	{
+		if ($Rebuild)
+		{
+			Invoke-Expression "nmake rebuild-release"
+		}
+		else
+		{
+			Invoke-Expression "nmake release"
+		}
 	}
 	else
 	{
-		Invoke-Expression "nmake release"
+		if ($Rebuild)
+		{
+			Invoke-Expression "nmake rebuild"
+		}
+		else
+		{
+			Invoke-Expression "nmake"
+		}
 	}
 }
-else
+elseif ($IsMacos)
 {
-	if ($Rebuild)
+	if ($Release)
 	{
-		Invoke-Expression "nmake rebuild"
+		Write-Output "Release builds are not supported for MacOS. Building debug instead."
+	}
+
+	if ($Clean)
+	{
+		Invoke-Expression "make clean"
+	}
+	elseif ($Rebuild)
+	{
+		Invoke-Expression "make rebuild-macos"
 	}
 	else
 	{
-		Invoke-Expression "nmake"
+		Invoke-Expression "make build-macos"
 	}
 }
--- makefile
+++ makefile
@@ -1,12 +1,17 @@
-DIR_SOURCE = ..\Source
 DIR_DEBUG = .\Debug
 DIR_RELEASE = .\Release
 
-CL_DEBUG_FLAGS = /Zi /FS /EHsc /nologo /DUNICODE /D_UNICODE /DEBUG /DDEBUG
-CL_RELEASE_FLAGS = /GL /O2 /Zi /FS /EHsc /nologo /DUNICODE /D_UNICODE
-OUTPUT_EXE = Bletris.exe
+# Windows
+CL_DEBUG_FLAGS = /Zi /FS /EHsc /nologo /DUNICODE /D_UNICODE /DEBUG /DDEBUG /DWINDOWS
+CL_RELEASE_FLAGS = /GL /O2 /Zi /FS /EHsc /nologo /DUNICODE /D_UNICODE /DWINDOWS
+OUTPUT_EXE_WINDOWS = Bletris.exe
 
-SOURCE_FILES = $(DIR_SOURCE)\cpp\*.cpp
+# MacOS
+CLANG_DEBUG_FLAGS = -std=c++14 -stdlib=libc++
+OUTPUT_MACOS = Bletris
+
+SOURCE_FILES = ..\Source\cpp\*.cpp
+SOURCE_FILES_MACOS = ../Source/cpp/*.cpp
 
 all: build
 
@@ -19,15 +24,23 @@ build:
 	@echo Building Debug...
 	@if not exist $(DIR_DEBUG) mkdir $(DIR_DEBUG)
 	@cd $(DIR_DEBUG)
-	cl.exe $(CL_DEBUG_FLAGS) /Fe:$(OUTPUT_EXE) $(SOURCE_FILES)
+	cl.exe $(CL_DEBUG_FLAGS) /Fe:$(OUTPUT_EXE_WINDOWS) $(SOURCE_FILES)
+
+build-macos:
+	@echo Building Debug-MacOS...
+	@mkdir -p ./Debug
+	cd Debug; \
+	clang $(CLANG_DEBUG_FLAGS) -g $(SOURCE_FILES_MACOS) -o $(OUTPUT_MACOS)
 
 release:
 	@echo Building Release...
 	@if not exist $(DIR_RELEASE) mkdir $(DIR_RELEASE)
 	@cd $(DIR_RELEASE)
-	cl.exe $(CL_RELEASE_FLAGS) /Fe:$(OUTPUT_EXE) $(SOURCE_FILES)
+	cl.exe $(CL_RELEASE_FLAGS) /Fe:$(OUTPUT_EXE_WINDOWS) $(SOURCE_FILES)
 
 rebuild: clean build
 rebuild-release: clean release
+rebuild-macos: clean build-macos
 
 Bletris.exe: rebuild-release
+Bletris: build-macos
